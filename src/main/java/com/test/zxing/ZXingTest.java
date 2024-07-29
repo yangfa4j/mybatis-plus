3b5a9b56d33f93449a776a2d1b9318bf028b448f
wb_yangfa <wb_yangfa@meituan.com>
2024-07-29 17:19:11 +0800

    zxing 生成二维码

--- src/main/java/com/test/zxing/ZXingTest.java
+++ src/main/java/com/test/zxing/ZXingTest.java
@@ -7,6 +7,7 @@ import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import org.apache.curator.shaded.com.google.common.base.Stopwatch;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -14,18 +15,23 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class ZXingTest {
 
-    private static int width = 935;
-    private static int height = 935;
+    private static int width = 200;
+    private static int height = 200;
 
     private static int margin = 0;   //白边大小，取值范围0~4
     private static ErrorCorrectionLevel level = ErrorCorrectionLevel.H;  //二维码容错率
 
-    public static void main(String[] args)  throws Exception{
-        BufferedImage encode = ZXingTest.encode("https://www.zhihu.com", width, height);
+    public static void main(String[] args) throws Exception {
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        String content = "https://payment2.m.1688.com/page/batchPaymentCashier.html?cashierOrderNo=3D04EB5820254EB3E52197427EB75749";
+        BufferedImage encode = ZXingTest.encode(content, width, height);
         ImageIO.write(encode, "png", new File("/Users/yangfa/Desktop/qrcode.png"));
+        stopwatch.stop();
+        System.out.println("执行时间: " + stopwatch.elapsed(TimeUnit.SECONDS) + " 秒");
     }
 
     public static BufferedImage encode(String content, int width, int height) throws WriterException, IOException {
